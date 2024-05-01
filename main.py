c793e06f1fc3026f31fc795d926bdef13dce1de2
Doug <doug@mimicit.com>
2024-05-01 08:24:14 -0700

    first test push 1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi cloudwarriors!'
+    return 'Hi cloudwarriors is cool!'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
